f0db94b35ac78f5389172ca14466033a117d0cf2
Karol Misdziol <karolstb@gmail.com>
2018-06-30 19:44:48 +0200

    pobieranie obrazka

--- ASPNetIdentity/Tools/Utils.cs
+++ ASPNetIdentity/Tools/Utils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
@@ -55,10 +56,87 @@ namespace MusicCollector.Tools
 
             return string.Format(pattern, max);
         }
+
+        //https://stackoverflow.com/questions/27846337/select-and-download-random-image-from-google#
+        #region pobierania obrazka z google
+        /// <summary>
+        /// zwraca zdjęcie z wynikow google na podstawie podanego hasła i które tam z kolejnosci
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <returns></returns>
+        public static byte[] SearchForGoogleImage(string topic, int order = 1)
+        {
+            string html = GetHtmlCode(topic);
+            string url = GetUrl(html, order);
+            byte[] image = GetImage(url);
+            return image;
+        }
+
+        private static string GetHtmlCode(string topic)
+        {
+            string url = "https://www.google.com/search?q=" + topic + "&tbm=isch";
+            string data = "";
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                if (dataStream == null)
+                    return "";
+                using (var sr = new StreamReader(dataStream))
+                {
+                    data = sr.ReadToEnd();
+                }
+            }
+            return data;
+        }
+
+        private static string GetUrl(string html, int order)
+        {
+            string url = "";
+            int i = 1;
+            int ndx = html.IndexOf("\"ou\"", StringComparison.Ordinal);
+
+            while (ndx >= 0 && i<=order)
+            {
+                ndx = html.IndexOf("\"", ndx + 4, StringComparison.Ordinal);
+                ndx++;
+                int ndx2 = html.IndexOf("\"", ndx, StringComparison.Ordinal);
+                url = html.Substring(ndx, ndx2 - ndx);
+                ndx = html.IndexOf("\"ou\"", ndx2, StringComparison.Ordinal);
+                i++;
+            }
+            return url;
+        }
+
+        private static byte[] GetImage(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                if (dataStream == null)
+                    return null;
+                using (var sr = new BinaryReader(dataStream))
+                {
+                    byte[] bytes = sr.ReadBytes(100000000);
+
+                    return bytes;
+                }
+            }
+
+            return null;
+        }
+        #endregion
     }
 
 
-    
+
 
     public class UtilsModel
     {
