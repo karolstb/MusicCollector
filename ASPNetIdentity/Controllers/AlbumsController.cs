7bbb21bbb6571451a7e1422dfd48b27b7ae83b8a
Karol Misdziol <karolstb@gmail.com>
2018-07-05 15:17:27 +0200

    prace nad obrazkami

--- ASPNetIdentity/Controllers/AlbumsController.cs
+++ ASPNetIdentity/Controllers/AlbumsController.cs
@@ -7,12 +7,13 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MusicCollector.Models;
+using System.IO;
 
 namespace MusicCollector.Controllers
 {
     public class AlbumsController : Controller
     {
-        //int orderNo = 1;
+        string coverImgBase64 = "";
         private MyApplicationDbContext db = new MyApplicationDbContext();
 
         // GET: Albums
@@ -59,7 +60,7 @@ namespace MusicCollector.Controllers
         [ValidateAntiForgeryToken]
         //public ActionResult Create([Bind(Include = "EntryNo,Author,Title,Duration")] Album album)   
         //public ActionResult Create([Bind(Include = "Author,Title,Duration,YearOfProduction", Prefix ="Duration")] Album album)
-        public ActionResult Create(Album album)
+        public ActionResult Create(Album album, string CoverArtHidden)
         {
             //bind include (albo exclude) definiuje kolumny, jakie można ustawić daną funkcją. 
             //dzięki temu zaboiega sie próbom atakowania np. przez fidller, gdzię można przypisać wartości do propertisów, 
@@ -68,9 +69,36 @@ namespace MusicCollector.Controllers
             {
                 db.Album.Add(album);
                 db.SaveChanges();
+
+                if (!String.IsNullOrEmpty(CoverArtHidden))
+                {
+                    Photo photo = new Photo();
+                    photo.AlbumNo = album.EntryNo;
+                    photo.IsMain = true;
+                    photo.UserUploaded = HttpContext.User.Identity.Name;
+
+                    //zapis obrazka na dysku do pliku
+                    if (!Directory.Exists(MusicCollector.Properties.Resources.PHOTO_PATH + "/" + album.EntryNo))
+                        Directory.CreateDirectory(MusicCollector.Properties.Resources.PHOTO_PATH + "/" + album.EntryNo);
+                    string fileName = DateTime.Now.Subtract(DateTime.MinValue.AddYears(1969)).TotalMilliseconds + ".jpg";
+                    string filePath = Path.Combine(MusicCollector.Properties.Resources.PHOTO_PATH + "/" + album.EntryNo, fileName);
+                    filePath = filePath.Replace(@"\", "/");
+
+                    var bytes = Convert.FromBase64String(CoverArtHidden);
+                    using (var imageFile = new FileStream(filePath, FileMode.Create))
+                    {
+                        imageFile.Write(bytes, 0, bytes.Length);
+                        imageFile.Flush();
+                    }
+
+                    photo.FilePath = album.EntryNo + "/" + fileName;
+                    db.Photos.Add(photo);
+                    db.SaveChanges();
+                }
+
                 return RedirectToAction("Index");
             }
-
+            
             return View(album);
         }
 
@@ -131,6 +159,21 @@ namespace MusicCollector.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
+            //usun zdjęcia
+            List<Photo> photoList = db.Photos.Where(c => c.AlbumNo == id).ToList();
+            foreach(Photo photo in photoList)
+            {
+                if (!String.IsNullOrEmpty(photo.FilePath))
+                {
+                    PhotosController photoController = new PhotosController();
+                    photoController.DeleteFile(Path.Combine(MusicCollector.Properties.Resources.PHOTO_PATH + "/", photo.FilePath));
+                }
+                db.Photos.Remove(photo);
+            }
+            //usun folder na zdjecia
+            if (Directory.Exists(MusicCollector.Properties.Resources.PHOTO_PATH + "/" + id))
+                Directory.Delete(MusicCollector.Properties.Resources.PHOTO_PATH + "/" + id);
+
             Album album = db.Album.Find(id);
             db.Album.Remove(album);
             db.SaveChanges();
@@ -159,14 +202,14 @@ namespace MusicCollector.Controllers
                 var emptyData = new { ImageBase64 = "" };
                 return Json(emptyData, JsonRequestBehavior.AllowGet);
             }
-
-            //orderNo = order;
+            
             ViewBag.OrderNo = order;
-
+            
             string photo = "R0lGODlhPQBEAPeoAJosM//AwO/AwHVYZ/z595kzAP/s7P+goOXMv8+fhw/v739/f+8PD98fH/8mJl+fn/9ZWb8/PzWlwv///6wWGbImAPgTEMImIN9gUFCEm/gDALULDN8PAD6atYdCTX9gUNKlj8wZAKUsAOzZz+UMAOsJAP/Z2ccMDA8PD/95eX5NWvsJCOVNQPtfX/8zM8+QePLl38MGBr8JCP+zs9myn/8GBqwpAP/GxgwJCPny78lzYLgjAJ8vAP9fX/+MjMUcAN8zM/9wcM8ZGcATEL+QePdZWf/29uc/P9cmJu9MTDImIN+/r7+/vz8/P8VNQGNugV8AAF9fX8swMNgTAFlDOICAgPNSUnNWSMQ5MBAQEJE3QPIGAM9AQMqGcG9vb6MhJsEdGM8vLx8fH98AANIWAMuQeL8fABkTEPPQ0OM5OSYdGFl5jo+Pj/+pqcsTE78wMFNGQLYmID4dGPvd3UBAQJmTkP+8vH9QUK+vr8ZWSHpzcJMmILdwcLOGcHRQUHxwcK9PT9DQ0O/v70w5MLypoG8wKOuwsP/g4P/Q0IcwKEswKMl8aJ9fX2xjdOtGRs/Pz+Dg4GImIP8gIH0sKEAwKKmTiKZ8aB/f39Wsl+LFt8dgUE9PT5x5aHBwcP+AgP+WltdgYMyZfyywz78AAAAAAAD///8AAP9mZv///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAKgALAAAAAA9AEQAAAj/AFEJHEiwoMGDCBMqXMiwocAbBww4nEhxoYkUpzJGrMixogkfGUNqlNixJEIDB0SqHGmyJSojM1bKZOmyop0gM3Oe2liTISKMOoPy7GnwY9CjIYcSRYm0aVKSLmE6nfq05QycVLPuhDrxBlCtYJUqNAq2bNWEBj6ZXRuyxZyDRtqwnXvkhACDV+euTeJm1Ki7A73qNWtFiF+/gA95Gly2CJLDhwEHMOUAAuOpLYDEgBxZ4GRTlC1fDnpkM+fOqD6DDj1aZpITp0dtGCDhr+fVuCu3zlg49ijaokTZTo27uG7Gjn2P+hI8+PDPERoUB318bWbfAJ5sUNFcuGRTYUqV/3ogfXp1rWlMc6awJjiAAd2fm4ogXjz56aypOoIde4OE5u/F9x199dlXnnGiHZWEYbGpsAEA3QXYnHwEFliKAgswgJ8LPeiUXGwedCAKABACCN+EA1pYIIYaFlcDhytd51sGAJbo3onOpajiihlO92KHGaUXGwWjUBChjSPiWJuOO/LYIm4v1tXfE6J4gCSJEZ7YgRYUNrkji9P55sF/ogxw5ZkSqIDaZBV6aSGYq/lGZplndkckZ98xoICbTcIJGQAZcNmdmUc210hs35nCyJ58fgmIKX5RQGOZowxaZwYA+JaoKQwswGijBV4C6SiTUmpphMspJx9unX4KaimjDv9aaXOEBteBqmuuxgEHoLX6Kqx+yXqqBANsgCtit4FWQAEkrNbpq7HSOmtwag5w57GrmlJBASEU18ADjUYb3ADTinIttsgSB1oJFfA63bduimuqKB1keqwUhoCSK374wbujvOSu4QG6UvxBRydcpKsav++Ca6G8A6Pr1x2kVMyHwsVxUALDq/krnrhPSOzXG1lUTIoffqGR7Goi2MAxbv6O2kEG56I7CSlRsEFKFVyovDJoIRTg7sugNRDGqCJzJgcKE0ywc0ELm6KBCCJo8DIPFeCWNGcyqNFE06ToAfV0HBRgxsvLThHn1oddQMrXj5DyAQgjEHSAJMWZwS3HPxT/QMbabI/iBCliMLEJKX2EEkomBAUCxRi42VDADxyTYDVogV+wSChqmKxEKCDAYFDFj4OmwbY7bDGdBhtrnTQYOigeChUmc1K3QTnAUfEgGFgAWt88hKA6aCRIXhxnQ1yg3BCayK44EWdkUQcBByEQChFXfCB776aQsG0BIlQgQgE8qO26X1h8cEUep8ngRBnOy74E9QgRgEAC8SvOfQkh7FDBDmS43PmGoIiKUUEGkMEC/PJHgxw0xH74yx/3XnaYRJgMB8obxQW6kL9QYEJ0FIFgByfIL7/IQAlvQwEpnAC7DtLNJCKUoO/w45c44GwCXiAFB/OXAATQryUxdN4LfFiwgjCNYg+kYMIEFkCKDs6PKAIJouyGWMS1FSKJOMRB/BoIxYJIUXFUxNwoIkEKPAgCBZSQHQ1A2EWDfDEUVLyADj5AChSIQW6gu10bE/JG2VnCZGfo4R4d0sdQoBAHhPjhIB94v/wRoRKQWGRHgrhGSQJxCS+0pCZbEhAAOw==";
             var imageBytes = Tools.Utils.SearchForGoogleImage(author + " " + title, order);
             photo = Convert.ToBase64String(imageBytes);
             var photoData = new { ImageBase64 = photo };
+            ViewBag.CoverImgBase64 = photo;
             return Json(photoData, JsonRequestBehavior.DenyGet);
             //var Releases = db.Release.Where(c => c.AlbumNo == albumNo).ToList();
             //if (!Releases.Any())
